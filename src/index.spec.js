72ad65068287064e740f5f4b6460f148bb4689a0
Michael Scott Hertzberg <mshertzberg@gmail.com>
2016-04-13 21:40:06 -0400

    living a lie, living a lie, timmy.

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,10 +1,11 @@
 import * as tap from 'tap'
-const it = tap.test
-const eventuallyTrue = () =>
-  new Promise(resolve => {
-    setTimeout(() => resolve(true), 100)
+const describe = tap.test
+const eventuallyTrue = () => new Promise(resolve => setTimeout(resolve(true), 1333.666))
+describe('can nest', i => {
+  const it = i.test
+  it('is true', async t => {
+    t.equal(await eventuallyTrue(), true)
+    t.end()
   })
-it('is true', async t => {
-  t.equal(await eventuallyTrue(), true)
-  t.end()
+  i.end()
 })
