5ecb0c1aafec3faca580de0bd82ac366a6144f42
Michael Scott Hertzberg <mshertzberg@gmail.com>
2016-04-14 19:35:40 -0400

    add test watcher script

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,10 +1,13 @@
 import * as tap from 'tap'
 const describe = tap.test
 const eventuallyTrue = () => new Promise(resolve => setTimeout(resolve(true), 1333.666))
-describe('can nest', i => {
+const eventuallyFalse = () => new Promise(resolve => setTimeout(resolve(false), 666.333))
+describe('PACKAGE_NAME', i => {
   const it = i.test
-  it('is true', async t => {
-    t.equal(await eventuallyTrue(), true)
+  it('Logic', async t => {
+    t.plan(2)
+    t.equal(await eventuallyTrue(), true, 'will eventually be true')
+    t.equal(await eventuallyFalse(), false, 'will eventually be false')
     t.end()
   })
   i.end()
