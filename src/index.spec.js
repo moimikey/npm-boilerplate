46e484e03c985fe29b1d7aa27bf1fce4f86824c6
Michael Scott Hertzberg <mshertzberg@gmail.com>
2016-06-27 18:22:46 -0400

    simplify test boilerplate

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,14 +1,17 @@
-import * as tap from 'tap'
+import { typeov } from 'typeov'
+import tap from 'tap'
+
 const describe = tap.test
-const eventuallyTrue = () => new Promise(resolve => setTimeout(resolve(true), 1333.666))
-const eventuallyFalse = () => new Promise(resolve => setTimeout(resolve(false), 666.333))
+
 describe('PACKAGE_NAME', i => {
   const it = i.test
+
   it('Logic', async t => {
-    t.plan(2)
-    t.equal(await eventuallyTrue(), true, 'will eventually be true')
-    t.equal(await eventuallyFalse(), false, 'will eventually be false')
+    // t.plan(2)
+    t.equal(true, true)
     t.end()
   })
+
   i.end()
+
 })
