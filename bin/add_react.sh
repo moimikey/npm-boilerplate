230f0c1d616464838ea9e1ab4e56b941ae6d3201
Michael Scott Hertzberg <mshertzberg@gmail.com>
2016-07-08 13:42:24 -0400

    add lave

---
+++ bin/add_react.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+#npm install --silent --save-dev eslint-plugin-react
+echo "
+@############################
+// add to .eslintrc
+{
+  \"plugins\": [
+    \"react\"
+  ]
+}
+@############################
+"
