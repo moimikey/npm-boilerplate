a7c418da0ebdd17a6ec7d199deb1aa5b29a82e51
Michael Scott Hertzberg <mshertzberg@gmail.com>
2017-04-22 15:01:42 -0400

    next version with yarn and flow

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,9 +1,11 @@
-import tap from 'tap'
-import { typeov } from 'typeov'
+/* @flow */
+
+const tap = require('tap')
 
 const test = tap.test
 
 test('the truth', t => {
+  t.cheese.t()
   t.equal(true, true, 'shall be known')
   t.end()
 })
