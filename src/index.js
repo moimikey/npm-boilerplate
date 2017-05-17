417f4885fa27ddedc940ae13f9b88c41d0f0f35c
Michael Scott Hertzberg <mshertzberg@gmail.com>
2017-05-17 17:37:49 -0400

    facelift!

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 /* @flow */
 
-module.exports = () => {}
+(function() {
+  function fib(x) {
+    return x <= 1 ? x : fib(x - 1) + fib(x - 2)
+  }
+  let x = Date.now()
+  if (x * 2 > 42) x = fib(10)
+  var a = x
+  console.log(a)
+})()
